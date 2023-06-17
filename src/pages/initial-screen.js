05911a8a9ee85127bcfbd5602d4e42a4a4f2a146
Sandeep Dokala <sandeepdokala@gmail.com>
2023-06-17 12:52:37 +0530

    Initialized firebase

--- src/pages/initial-screen.js
+++ src/pages/initial-screen.js
@@ -1,5 +1,20 @@
+import { addDoc, getDocs, collection } from "@firebase/firestore";
+import { db } from "../firebase";
+import { useEffect } from "react";
 
 export default function InitialScreen() {
+    
+    const getData = async () => {
+        await getDocs(collection(db, "restaurant_details"))
+            .then((query) => {
+                const data = query.docs.map((doc) => ({...doc.data(), id:doc.id }));
+                console.log(data)
+            })
+    }
+
+    useEffect( () => {
+        getData();
+    }, [] )
 
     return(
         <div className="initial-screen-container">
